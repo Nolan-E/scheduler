0e4b5662f703103de62a21cd1917a6c79a7b2ab7
Nolan Eckert <nolan.eckert@gmail.com>
2021-03-16 15:25:45 -0600

    Update interviewerList component.:

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,14 +1,25 @@
 import React from 'react';
 
 import 'components/InterviewerList.scss';
-import className from 'classnames';
+import InterviewerListItem from './InterviewerListItem';
 
 export default function InterviewerList(props) {
-  let itemClass = className();
+  const listArr = props.interviewers.map(inter => {
+    return (
+      <InterviewerListItem
+        key={inter.id}
+        id={inter.id}
+        name={inter.name}
+        avatar={inter.avatar}
+        selected={inter.id === props.interviewer}
+        setInterviewer={props.setInterviewer}
+      />
+    );
+  });
   return (
-    <section className="interviewers">
+    <section>
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">{listArr}</ul>
     </section>
   );
 }
