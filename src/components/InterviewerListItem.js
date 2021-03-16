8fc20281e46e0f5de249d224fe2d3d754735eeaf
Nolan Eckert <nolan.eckert@gmail.com>
2021-03-15 22:52:23 -0600

    Add InterviewerListItem component, styling and story.

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import 'components/InterviewerListItem.scss';
+import className from 'classnames';
+export default function InterviewerListItem(props) {
+  let itemClass = className('interviewers__item', {
+    'interviewers__item--selected': props.selected
+  });
+  return (
+    <li className={itemClass} onClick={() => props.setInterviewer(props.name)}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
+  );
+}
