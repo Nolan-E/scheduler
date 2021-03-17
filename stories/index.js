2044a5c6307f1c13cdd7bed056b9c43daeb94080
Nolan Eckert <nolan.eckert@gmail.com>
2021-03-16 18:02:32 -0600

    Add Show component & story.

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,7 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 // Button Component
 storiesOf("Button", module)
@@ -147,4 +148,12 @@ storiesOf("Appointment", module)
     />
   ))
   .add("Header", () => <Header time="12pm" />)
-  .add("Empty", () => <Empty onAdd={action("onAdd")} />);
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+  .add("Show", () => (
+    <Show
+      student="Lydia Miller-Jones"
+      interviewer={interviewer}
+      onEdit={action("onEdit")}
+      onDelete={action("onDelete")}
+    />
+  ));
