8ebd1801c70074984381d132d46791d7ead5222e
Nolan Eckert <nolan.eckert@gmail.com>
2021-03-16 22:23:00 -0600

    Add appointment rendering.

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,6 +3,46 @@ import React, { useState } from "react";
 import "components/Application.scss";
 
 import DayList from 'components/DayList';
+import Appointment from 'components/Appointment/index';
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Goku",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png"
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+  }
+];
 
 const days = [
   {
