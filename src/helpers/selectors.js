fa64d2e8c9c252db9367801490125061d0caba94
Nolan Eckert <nolan.eckert@gmail.com>
2021-03-17 23:49:27 -0600

    Update selectors helper to implement getInterview and test

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,4 +1,8 @@
 export function getAppointmentsForDay(state, day) {
   const filterDays = state.days.find(currDay => currDay.name === day);
   return !filterDays ? [] : filterDays.appointments.map(apptID => state.appointments[apptID]);
-}
+};
+
+export function getInterview(state, interview) {
+  return !interview ? null : {interviewer: state.interviewers[interview.interviewer], student: interview.student};
+};
