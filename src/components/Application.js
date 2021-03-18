1d517f57dc36bc21258e9511214b7bfc72a50cee
Nolan Eckert <nolan.eckert@gmail.com>
2021-03-17 18:05:38 -0600

    Refactor multiple useState hooks into one generic one.

--- src/components/Application.js
+++ src/components/Application.js
@@ -64,15 +64,21 @@ const appointments = [
 ];*/
 
 export default function Application(props) {
-  const [day, setDay] = useState('Monday');
-  const [days, setDays] = useState([]);
+  // const [day, setDay] = useState('Monday');
+  // const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: 'Monday',
+    days: [],
+    appointments: {}
+  });
+  const setDay = day => setState({...state, day});
+  const setDays = days => setState(prev => ({...prev, days}));
+
   useEffect(() => {
     axios.get('/api/days')
-      .then(res => setDays([...res.data]));
-    // return () => {
-    //   cleanup
-    // }
+      .then(res => setDays(res.data));
   }, [])
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -85,8 +91,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            day={day}
+            days={state.days}
+            day={state.day}
             setDay={setDay}
           />
         </nav>
