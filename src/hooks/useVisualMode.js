2330c93a9c7c867be6cebb6749e37f0f9e9cfe54
Nolan Eckert <nolan.eckert@gmail.com>
2021-03-19 16:43:36 -0600

    Refactor useVisualMode to ensure no stale state.

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,26 +1,26 @@
 import { useState } from 'react';
 
 export default function useVisualMode(init) {
-  const [mode, setMode] = useState(init);
   const [history, setHistory] = useState([init]);
 
   const transition = (newMode, replace = false) => {
     if (replace) {
-      history.pop();
-      setMode(newMode);
+      setHistory(prev => [...prev.slice(0,1), newMode]);
+    } else {
+      setHistory(prev => [...prev, newMode]);
     }
-    setMode(newMode);
-    const newHist = [...history];
-    newHist.push(newMode);
-    setHistory(newHist);
   };
   const back = () => {
     if (history.length < 2) {
       return;
     }
-    setMode(history[history.length-2]);
-    setHistory(prev => prev.slice(0, -1));
+    setHistory(prev => {
+      const newHist = [...prev];
+      newHist.pop();
+      return newHist;
+    });
   };
   
+  const mode = (history.slice(-1)[0]);
   return {mode, transition, back, history};
-}
+};
