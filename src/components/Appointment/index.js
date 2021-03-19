9ccbe8c7295a00a4c981efd39c8ab79eb8d7ba0a
Nolan Eckert <nolan.eckert@gmail.com>
2021-03-19 16:13:07 -0600

    Add save & delete error component rendering and logic in .catch.

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -7,6 +7,7 @@ import Show from 'components/Appointment/Show';
 import Form from 'components/Appointment/Form';
 import Status from 'components/Appointment/Status';
 import Confirm from 'components/Appointment/Confirm';
+import Error from 'components/Appointment/Error';
 import useVisualMode from 'hooks/useVisualMode';
 
 export default function Appointment(props) {
@@ -17,6 +18,8 @@ export default function Appointment(props) {
   const DELETING = 'DELETING';
   const CONFIRM = 'CONFIRM';
   const EDIT = 'EDIT';
+  const ERROR_SAVE = 'ERROR_SAVE';
+  const ERROR_DELETE = 'ERROR_DELETE';
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
@@ -30,7 +33,7 @@ export default function Appointment(props) {
     transition(SAVING)
     props.bookInterview(props.id, interview)
       .then(() => transition(SHOW))
-      // .catch(err => console.log(err))
+      .catch(() => transition(ERROR_SAVE))
   }
 
   const deleteInterview = () => {
@@ -41,6 +44,7 @@ export default function Appointment(props) {
     transition(DELETING)
     props.cancelInterview(props.id)
       .then(() => transition(EMPTY))
+      .catch(() => transition(ERROR_DELETE))
   }
 
   const editInterview = () => {
@@ -84,6 +88,8 @@ export default function Appointment(props) {
           onSave={save}
         />
       )}
+      {mode === ERROR_SAVE && <Error message="Could not save appointment." onClose={back} />}
+      {mode === ERROR_DELETE && <Error message="Could not delete appointment." onClose={back} />}
     </article>
   );
 };
