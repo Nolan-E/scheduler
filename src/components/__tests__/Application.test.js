865110da9468adeba3f85f1223569f8701337101
Nolan Eckert <nolan.eckert@gmail.com>
2021-03-23 17:59:51 -0600

    Update test to include first 4 steps in cancel interview integration test.

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -7,6 +7,7 @@ import {
   getByPlaceholderText,
   getByText,
   prettyDOM,
+  queryByAltText,
   queryByText,
   render,
   waitForElement,
@@ -28,7 +29,7 @@ describe("Application", () => {
   });
 
   it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
-    const { container, debug } = render(<Application />);
+    const { container } = render(<Application />);
 
     await waitForElement(() => getByText(container, "Archie Cohen"));
 
@@ -53,5 +54,28 @@ describe("Application", () => {
     );
 
     expect(getByText(day, "no spots remaining")).toBeInTheDocument();
+  });
+
+  it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+    // 1. Render the Application
+    const { container, debug } = render(<Application />);
+    // 2. Wait until the text "Archie Cohen" is displayed
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+    // 3. Click the "Delete" button on the booked appointment.
+    const appointment = getAllByTestId(
+      container,
+      "appointment"
+    ).find((appointment) => queryByText(appointment, "Archie Cohen"));
+
+    fireEvent.click(queryByAltText(appointment, "Delete"));
+    // 4. Check that the confirmation message appears.
+    expect(getByText(appointment, "Delete the appointment?")).toBeInTheDocument();
+    // 5. Click the "Confirm" button on the confirmation.
+    
+    // 6. Check that the element with the text "Deleting" is displayed.
+    // 7. Wait until the element with the "Add" button is displayed.
+    // 8. Check that the DayListItem with the text "Monday" also has the text "2 spots remaining".
+
+    debug();
   });
 });
