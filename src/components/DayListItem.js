e0ea5b088e681b73d00a62401c104036ff044545
Nolan Eckert <nolan.eckert@gmail.com>
2021-03-23 17:03:53 -0600

    Add tests for saving status and to check no spots remaining appears at the end of the integration function.

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,25 +4,29 @@ import "components/DayListItem.scss";
 import className from "classnames";
 
 export default function DayListIem(props) {
-  let itemClass = className('day-list__item', {
-    'day-list__item--selected': props.selected,
-    'day-list__item--full': props.spots === 0
+  let itemClass = className("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0,
   });
 
   const formatSpots = (spots) => {
     if (spots === 0) {
-      return 'no spots remaining';
+      return "no spots remaining";
     }
 
     if (spots === 1) {
-      return '1 spot remaining';
+      return "1 spot remaining";
     }
 
     return `${spots} spots remaining`;
   };
 
   return (
-    <li className={itemClass} onClick={() => props.setDay(props.name)}>
+    <li
+      className={itemClass}
+      onClick={() => props.setDay(props.name)}
+      data-testid="day"
+    >
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
