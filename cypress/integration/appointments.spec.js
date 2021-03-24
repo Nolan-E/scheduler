3f117267730ddda3325c91ab0a16347b81c1065a
Nolan Eckert <nolan.eckert@gmail.com>
2021-03-24 16:36:33 -0600

    Add cancel interview E2E test.

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -6,6 +6,7 @@ describe("Appointments", () => {
   
     cy.contains('Monday')
   });
+
   it("should book an interview", () => {
     cy.get('[alt=Add]')
       .first()
@@ -21,7 +22,24 @@ describe("Appointments", () => {
     .click();
 
     cy.contains('.appointment__card--show', 'Lydia Miller-Jones');
-
     cy.contains('.appointment__card--show', 'Sylvia Palmer');
+  });
+
+  it("should edit an interview", () => {
+    cy.get('[alt=Edit]')
+      .first()
+      .click({force: true});
+
+    cy.get('[data-testid=student-name-input]')
+      .type('{selectAll}{backspace}Lydia Miller-Jones');
+
+    cy.get('[alt="Tori Malcolm"]')
+      .click();
+
+    cy.contains('Save')
+      .click();
+
+    cy.contains('.appointment__card--show', 'Lydia Miller-Jones');
+    cy.contains('.appointment__card--show', 'Tori Malcolm');
   });
 });
